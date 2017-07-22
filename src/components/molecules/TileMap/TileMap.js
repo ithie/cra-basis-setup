5ce068bb91afe1e0a529d547665ad4440e8e58b3
Michael Draws <michael.draws@nexum.de>
2017-07-22 16:49:35 +0200

    chore(TileMap): Made the map-generation way more generic.

--- src/components/molecules/TileMap/TileMap.js
+++ src/components/molecules/TileMap/TileMap.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import _ from 'lodash';
+import tiles, { E, S, LF, F, DF, OC, SM, T, V } from '../../../constants/enums/Tiles';
 
 import TileProvider, { tileTypes } from '../../containers/TileProvider/TileProvider';
 
@@ -31,6 +33,14 @@ const Even = styled.div`
   }
 `;
 
+const tileMap = [
+  [E, S, S, S, S, E],
+  [E, S, LF, F, DF, S, E],
+  [S, SM, V, T, OC, S],
+  [E, S, LF, F, DF, S, E],
+  [E, S, S, S, S, E],
+];
+
 export default class TileMap extends Component {
   static displayName = 'molecules/TileMap';
 
@@ -40,57 +50,41 @@ export default class TileMap extends Component {
     this.state = { activeTile: null };
   }
 
+  getMap = () =>
+    React.createElement(
+      Map,
+      {},
+      _.map(tileMap, (row, rowIndex) =>
+        React.createElement(
+          rowIndex % 2 === 0 ? Odd : Even,
+          {
+            key: rowIndex,
+          },
+          _.map(row, (column, columnIndex) =>
+            React.createElement(
+              TileProvider,
+              {
+                key: `${rowIndex}:${columnIndex}`,
+                type: typeof tiles[column] !== 'undefined' ? tiles[column] : tileTypes.Empty,
+                handle: this.handle,
+                activeTile: this.state.activeTile,
+                halfTile: rowIndex % 2 === 1 && (columnIndex === 0 || columnIndex === row.length - 1),
+                left: rowIndex % 2 === 0 && columnIndex === 0,
+                right: rowIndex % 2 === 0 && columnIndex === row.length - 1,
+              },
+              ''
+            )
+          )
+        )
+      )
+    );
+
   handle = tile =>
     this.setState({
       activeTile: tile && tile !== this.state.activeTile ? tile : null,
     });
 
   render() {
-    return (
-      <Map>
-        <Odd>
-          <TileProvider handle={this.handle} activeTile={this.state.activeTile} type={tileTypes.Empty} />
-          <TileProvider handle={this.handle} activeTile={this.state.activeTile} type={tileTypes.Sea} />
-          <TileProvider handle={this.handle} activeTile={this.state.activeTile} type={tileTypes.Sea} />
-          <TileProvider handle={this.handle} activeTile={this.state.activeTile} type={tileTypes.Sea} />
-          <TileProvider handle={this.handle} activeTile={this.state.activeTile} type={tileTypes.Sea} />
-          <TileProvider handle={this.handle} activeTile={this.state.activeTile} type={tileTypes.Empty} />
-        </Odd>
-        <Even>
-          <TileProvider handle={this.handle} activeTile={this.state.activeTile} type={tileTypes.Empty} halfTile left />
-          <TileProvider handle={this.handle} activeTile={this.state.activeTile} type={tileTypes.Sea} />
-          <TileProvider handle={this.handle} activeTile={this.state.activeTile} type={tileTypes.LightForrest} />
-          <TileProvider handle={this.handle} activeTile={this.state.activeTile} type={tileTypes.Forrest} />
-          <TileProvider handle={this.handle} activeTile={this.state.activeTile} type={tileTypes.DarkForrest} />
-          <TileProvider handle={this.handle} activeTile={this.state.activeTile} type={tileTypes.Sea} />
-          <TileProvider handle={this.handle} activeTile={this.state.activeTile} type={tileTypes.Empty} halfTile right />
-        </Even>
-        <Odd>
-          <TileProvider handle={this.handle} activeTile={this.state.activeTile} type={tileTypes.Sea} />
-          <TileProvider handle={this.handle} activeTile={this.state.activeTile} type={tileTypes.Settlement} />
-          <TileProvider handle={this.handle} activeTile={this.state.activeTile} type={tileTypes.Village} />
-          <TileProvider handle={this.handle} activeTile={this.state.activeTile} type={tileTypes.Town} />
-          <TileProvider handle={this.handle} activeTile={this.state.activeTile} type={tileTypes.OpenCountry} />
-          <TileProvider handle={this.handle} activeTile={this.state.activeTile} type={tileTypes.Sea} />
-        </Odd>
-        <Even>
-          <TileProvider handle={this.handle} activeTile={this.state.activeTile} type={tileTypes.Empty} halfTile left />
-          <TileProvider handle={this.handle} activeTile={this.state.activeTile} type={tileTypes.Sea} />
-          <TileProvider handle={this.handle} activeTile={this.state.activeTile} type={tileTypes.LightForrest} />
-          <TileProvider handle={this.handle} activeTile={this.state.activeTile} type={tileTypes.Forrest} />
-          <TileProvider handle={this.handle} activeTile={this.state.activeTile} type={tileTypes.DarkForrest} />
-          <TileProvider handle={this.handle} activeTile={this.state.activeTile} type={tileTypes.Sea} />
-          <TileProvider handle={this.handle} activeTile={this.state.activeTile} type={tileTypes.Empty} halfTile right />
-        </Even>
-        <Odd>
-          <TileProvider handle={this.handle} activeTile={this.state.activeTile} type={tileTypes.Empty} />
-          <TileProvider handle={this.handle} activeTile={this.state.activeTile} type={tileTypes.Sea} />
-          <TileProvider handle={this.handle} activeTile={this.state.activeTile} type={tileTypes.Sea} />
-          <TileProvider handle={this.handle} activeTile={this.state.activeTile} type={tileTypes.Sea} />
-          <TileProvider handle={this.handle} activeTile={this.state.activeTile} type={tileTypes.Sea} />
-          <TileProvider handle={this.handle} activeTile={this.state.activeTile} type={tileTypes.Empty} />
-        </Odd>
-      </Map>
-    );
+    return this.getMap();
   }
 }
---
+++ src/constants/enums/Tiles.js
@@ -0,0 +1,25 @@
+import { tileTypes } from '../../components/containers/TileProvider/TileProvider';
+
+export const E = 'E';
+export const S = 'S';
+export const LF = 'LF';
+export const F = 'F';
+export const DF = 'DF';
+export const SM = 'SM';
+export const V = 'V';
+export const T = 'T';
+export const OC = 'OC';
+
+const tiles = {
+  [E]: tileTypes.Empty,
+  [S]: tileTypes.Sea,
+  [LF]: tileTypes.LightForrest,
+  [F]: tileTypes.Forrest,
+  [DF]: tileTypes.DarkForrest,
+  [SM]: tileTypes.Settlement,
+  [V]: tileTypes.Village,
+  [T]: tileTypes.Town,
+  [OC]: tileTypes.OpenCountry,
+};
+
+export default tiles;
