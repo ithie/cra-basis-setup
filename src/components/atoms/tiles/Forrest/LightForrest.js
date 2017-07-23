25190147f2d7ef7f33e45759bdcd50e39e1005c9
Michael Draws <michael.draws@nexum.de>
2017-07-23 13:34:02 +0200

    fix(LightForrest): Corrected typo.

--- src/components/atoms/tiles/Forrest/LightForrest.js
+++ src/components/atoms/tiles/Forrest/LightForrest.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 import Tile from '../Tile';
 
-const LightForrest = ({ isActive }) => <Tile color={'#9ACD32'} isActive={isActive}>Forrest</Tile>;
+const LightForrest = ({ isActive }) => <Tile color={'#9ACD32'} isActive={isActive}>Light Forrest</Tile>;
 
 LightForrest.propTypes = {
   isActive: PropTypes.bool,
